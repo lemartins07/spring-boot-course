f40c2f230c495f123bfe90bdfc40738594f2f6be
Leandro Martins <leandro.martins.85@gmail.com>
2021-10-12 23:02:32 -0300

    Project created

---
+++ src/test/java/com/educandoweb/springbootcourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
