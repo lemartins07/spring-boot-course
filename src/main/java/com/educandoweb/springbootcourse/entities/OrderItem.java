7c7eb5116f7b678bf2b7ba8c7eb377ba09d9d665
Leandro Martins <leandro.martins,85@gmail.com>
2021-10-14 19:49:02 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/springbootcourse/entities/OrderItem.java
+++ src/main/java/com/educandoweb/springbootcourse/entities/OrderItem.java
@@ -41,7 +41,7 @@ public class OrderItem implements Serializable {
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+		
 	public Product getProduct() {
 		return id.getProduct();
 	}
@@ -82,6 +82,4 @@ public class OrderItem implements Serializable {
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
